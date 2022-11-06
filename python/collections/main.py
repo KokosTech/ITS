6324b7d6d9f6419684bd6b070511dbd64780398f
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2022-11-06 17:12:11 +0200

    [ADD] Points

--- python/collections/main.py
+++ python/collections/main.py
@@ -1,3 +1,5 @@
+# EX1
+
 books = {"Life of Pi":"Adventure Fiction",
          "The three musketeers":"Historical Adventure",
          "Watchmen":"Comics",
@@ -7,3 +9,12 @@ books = {"Life of Pi":"Adventure Fiction",
 book = input()
 
 print(books.get(book, "Not found"))
+
+# EX2
+
+import math
+
+p1 = (8, 11)
+p2 = (25, 13)
+
+print(math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2))
